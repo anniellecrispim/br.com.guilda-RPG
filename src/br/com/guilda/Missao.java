0cafbb6aef0da76e88e94b8591f4a55cf80efe7c
Annielle <anniellemedeiros@gmail.com>
2020-11-23 17:51:55 -0300

    Missao construtor e atributos básicos

--- src/br/com/guilda/Missao.java
+++ src/br/com/guilda/Missao.java
@@ -1,4 +1,39 @@
 package br.com.guilda;
 
 public class Missao {
+
+    private String tituloDaMissao;
+    private int dificuldade;
+    private Item recompensa;
+    private String status;
+    private boolean disponivel;
+
+    public Missao(String tituloDaMissao,int dificuldade, Item recompensa) {
+        this.setTituloDaMissao(tituloDaMissao);
+        this.setDificuldade(dificuldade);
+        this.recompensa = recompensa;
+        this.status = "Disponível";
+        this.setDisponivel(true);
+    }
+
+    public int getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(int dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public void setTituloDaMissao(String tituloDaMissao) {
+        this.tituloDaMissao = tituloDaMissao;
+    }
+
 }
